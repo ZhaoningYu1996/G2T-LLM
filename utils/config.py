943a1baad353a4db8e98b2dd50156c8cc8357d07
ZhaoningYu1996 <zha0nin9@gmail.com>
2024-10-04 11:44:18 -0500

    Atom type and Bond type configs

---
+++ utils/config.py
@@ -0,0 +1,29 @@
+from enum import Enum, IntEnum
+
+class AtomTypeEnumZinc(str, Enum):
+    N = "N"
+    P = "P"
+    F = "F"
+    C = "C"
+    Br = "Br"
+    S = "S"
+    I = "I"
+    Cl = "Cl"
+    O = "O"
+
+class BondTypeEnumZinc(Enum):
+    SINGLE = 'SINGLE'
+    DOUBLE = 'DOUBLE'
+    TRIPLE = 'TRIPLE'
+    AROMATIC = 'AROMATIC'
+
+class AtomTypeEnumQM9(Enum):
+    O = 'O'
+    C = 'C'
+    F = 'F'
+    N = 'N'
+
+class BondTypeEnumQM9(Enum):
+    SINGLE = 'SINGLE'
+    DOUBLE = 'DOUBLE'
+    TRIPLE = 'TRIPLE'
